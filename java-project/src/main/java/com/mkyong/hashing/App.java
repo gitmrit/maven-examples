a6a5be890d5b9f29677bffc8aebce9e5b10675d1
git <git@mysururoyal.org>
2025-03-06 14:05:12 +0530

    commit

--- java-project/src/main/java/com/mkyong/hashing/App.java
+++ java-project/src/main/java/com/mkyong/hashing/App.java
@@ -6,11 +6,8 @@ public class App {
 
     public static void main(String[] args) {
 
-        if (args.length < 1) {
-            System.err.println("Please provide an input!");
-            System.exit(0);
-        }
-        System.out.println(sha256hex(args[0]));
+     
+        System.out.println("code is:"+sha256hex("1234"));
 
     }
 
